7b9600df941a78dfd0cd66c3d744814364d793b5
xwjahahahaha <1438575858@qq.com>
2022-07-18 18:59:27 +0800

    feat: make helloOS demo

---
+++ lessons/02/helloOS/main.c
@@ -0,0 +1,8 @@
+//
+// Created by 许文杰 on 2022/7/17.
+//
+#include "vgastr.h"
+int main() {
+    printf("Hello, World!");
+    return 0;
+}
---
+++ lessons/02/helloOS/vgastr.h
@@ -0,0 +1,6 @@
+//
+// Created by 许文杰 on 2022/7/17.
+//
+
+int _strwrite(char* string);
+int printf(char* fmt, ...);
